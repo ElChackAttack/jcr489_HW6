ea5d343b36f0c584264214c0df86c76f7e374323
Ricardo Chacon <ricardochacon@nyu.edu>
2017-03-23 14:06:34 +0800

    Bomus reverse stack finding smallest pancake

--- Bonus_student.py
+++ Bonus_student.py
@@ -5,59 +5,6 @@ random.seed(0)
 SIZE = 10
 LOGGING = True
 
-"""
-class PancakeStack():
-    def __init__(self, stack = None):
-        self.stack = stack
-
-    def get_size(self):
-        return len(self.stack)
-
-    # All of the pancakes are sorted after index
-    # Returns the index of largest unsorted pancake
-    def find_largest_pancake(self, index):
-        largest_index = index
-        #write your code here
-
-        return largest_index
-
-    # Slide the pan under pancake at desired index and flip to top
-    def flip(self, index):
-        #write your code here
-        return
-
-
-def sort_pancakes(pancakes):
-    pancakes_size = pancakes.get_size()
-    for i in reversed(range(pancakes_size)):
-        flip_index = pancakes.find_largest_pancake(i)
-        pancakes.flip(flip_index)
-        if LOGGING: print("Flip Up", pancakes.stack)
-        pancakes.flip(i)
-        if LOGGING: print("Flip Down", pancakes.stack)
-    return pancakes.stack
-
-
-if __name__ == "__main__":
-    my_stack = random.sample(range(1, 20), SIZE)
-    print("Unsorted pancakes:", my_stack)
-    case_one = PancakeStack(my_stack)
-    print("Final order of pancakes: ", sort_pancakes(case_one))
-"""
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Mar 13 15:02:35 2017
-
-@author: Mebius
-"""
-
-import random
-
-random.seed(0)
-
-SIZE = 10
-LOGGING = True
-
 
 class PancakeStack():
     def __init__(self, stack = None):
@@ -68,19 +15,19 @@ class PancakeStack():
 
     # All of the pancakes are sorted after index
     # Returns the index of largest unsorted pancake
-    def find_largest_pancake(self, index):
-        largest_pancake = self.stack[index]
-        largest_index = index
+    def find_next_smallest_pancake(self, index):
+        smallest_pancake = self.stack[index]
+        smallest_index = index
 
         for i in range(index):
-            if self.stack[i] > largest_pancake:
-                largest_pancake = self.stack[i]
-                largest_index = i
+            if self.stack[i] < smallest_pancake:
+                smallest_pancake = self.stack[i]
+                smallest_index = i
 
         if LOGGING:
-            print("Insert the pan at index %d with the largest in flip as %d" \
-                  %(largest_index, largest_pancake))
-        return largest_index
+            print("Insert the pan at index %d with the smallest in flip as %d" \
+                  %(smallest_index, smallest_pancake))
+        return smallest_index
 
     # Slide the pan under pancake at desired index and flip to top
     def flip(self, index):
@@ -94,11 +41,13 @@ class PancakeStack():
 def sort_pancakes(pancakes):
     pancakes_size = pancakes.get_size()
     for i in reversed(range(pancakes_size)):
-        flip_index = pancakes.find_largest_pancake(i)
+        flip_index = pancakes.find_next_smallest_pancake(i)
         pancakes.flip(flip_index)
         if LOGGING: print("Flip Up", pancakes.stack)
         pancakes.flip(i)
         if LOGGING: print("Flip Down", pancakes.stack)
+#        pancakes.flip(flip_index)
+    pancakes.stack.reverse()
     return pancakes.stack
 
 
