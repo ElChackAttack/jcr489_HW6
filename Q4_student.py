ea06a795049373d9d4af4e884878cf122e32f808
Ricardo Chacon <ricardochacon@nyu.edu>
2017-03-24 14:51:00 +0800

    The Bonus should be what they want me to do, and for the MaxSum of the triangles I only need to figure out where to do the comparison between two elements and it will be all hao

--- Q4_student.py
+++ Q4_student.py
@@ -8,7 +8,7 @@ Created on Tue Mar 14 14:11:20 2017
 import random
 random.seed(0)
 
-TRI_DEPTH = 2
+TRI_DEPTH = 4
 
 class Triangle:
 
@@ -16,7 +16,7 @@ class Triangle:
         self.up_t = up_t
         self.size = 1 if up_t == None else up_t.get_size() + 1
         self.my_row = [random.randint(0, 20) for i in range(self.size)]
-        self.maxsum = []
+        self.maxsum = []    
 
     def get_size(self):
         return self.size
@@ -25,22 +25,34 @@ class Triangle:
         #write your code here, and be sure to return what you've done
         maxsum = [0 for i in range(len(self.my_row))]
         if self.get_size() == 1:
-            maxsum[0] = self.my_row[0]
+            maxsum = self.my_row[:]  
+        else:
+            temp = self.up_t.get_maxsum()[:]
+            temp.extend(self.up_t.get_maxsum())
+            for i in range(self.size) :
+#           
+                to_add = temp[i] + self.my_row[i]
+                maxsum[i] = to_add
+            print(temp)
+        self.maxsum = maxsum
+        return self.maxsum
+"""
+Bruce said this part was redundant
+
         elif self.get_size() == 2:
             for i in range(2):
                 maxsum[i] = self.my_row[i]
         else:
-            maxsum[0] = self.up_t.get_maxsum()[0] + self.my_row[0]
+            maxsum[0] = self.up_t.get_maxsum()[:] + self.my_row[0]
             maxsum[-1] = self.up_t.get_maxsum()[-1] + self.my_row[-1]
             for i in range(1,self.size - 1):
                 if self.my_row[i] > self.my_row[i+1]:
                     maxsum[i] += self.my_row[i] + (self.up_t.get_maxsum())[i]
                 else:
                     maxsum[i] += self.my_row
-            
-            self.maxsum = maxsum
-        return self.maxsum
 
+            self.maxsum = maxsum
+"""
 def print_triangles(ts):
     for t in ts:
         print(t.my_row)
